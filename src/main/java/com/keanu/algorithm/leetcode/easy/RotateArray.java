346818a2181285f93d97be3d523b8a3a8930d90b
reiy-2001 <reiy-2001@itrade.co.jp>
2020-04-16 11:47:08 +0900

    189 - easy - RotateArray

--- src/main/java/com/keanu/algorithm/leetcode/easy/RotateArray.java
+++ src/main/java/com/keanu/algorithm/leetcode/easy/RotateArray.java
@@ -5,6 +5,18 @@ package com.keanu.algorithm.leetcode.easy;
  */
 public class RotateArray {
     public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
+    private void reverse(int[] nums, int start, int end) {
+        if (nums == null || nums.length <= 0 || start < 0 || end >= nums.length) return;
+        for (int i = start, j = end; i < j; i++, j--) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
     }
 }
